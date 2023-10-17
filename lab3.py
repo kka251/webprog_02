1c5605dbf3f7048f1e2f33e35fa0a26702b28b87
kka251 <k.kurilowa2018@yandex.ru>
2023-10-17 14:59:19 +0700

    Железнодорожный билет

--- lab3.py
+++ lab3.py
@@ -42,3 +42,46 @@ def pay():
 @lab3.route('/lab3/success')
 def success():
     return render_template('success.html')
+
+@lab3.route('/lab3/ticket')
+def ticket():
+    errors = {}
+    fio = request.args.get('fio')
+    if fio == '':
+        errors['fio'] = 'Заполните поле!'
+    tip = request.args.get('tip')
+    polka = request.args.get('polka')
+    bag = request.args.get('bag')
+    age = request.args.get('age')
+    if age == '':
+        errors['age'] = 'Заполните поле!'
+    viezd = request.args.get('viezd')
+    if viezd == '':
+        errors['viezd'] = 'Заполните поле!'
+    prib = request.args.get('prib')
+    if prib == '':
+        errors['prib'] = 'Заполните поле!'
+    date = request.args.get('date')
+    if prib == '':
+        errors['date'] = 'Заполните поле!'
+    return render_template('ticket.html', fio=fio, age=age, tip=tip, polka=polka, bag=bag, viezd=viezd, prib=prib, date=date, errors=errors)
+
+@lab3.route('/lab3/oform')
+def oform():
+    fio = request.args.get('fio')
+    tip = request.args.get('tip')
+    polka = request.args.get('polka')
+    bag = request.args.get('bag')
+    age = request.args.get('age')
+    viezd = request.args.get('viezd')
+    prib = request.args.get('prib')
+    date = request.args.get('date')
+    return render_template('oform.html', fio=fio, age=age, tip=tip, polka=polka, bag=bag, viezd=viezd, prib=prib, date=date)
+
+@lab3.route('/lab3/pay2')
+def pay2():
+    price2 = 1500
+    if request.args.get('bag') == 'yes':
+        price2 += 500
+    
+    return render_template('pay2.html', price2=price2)
