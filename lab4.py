12800f392fd8a0699b9c91f7db561cbc1b975f9d
kka251 <k.kurilowa2018@yandex.ru>
2023-10-24 18:27:12 +0700

    Холодильник

--- lab4.py
+++ lab4.py
@@ -26,9 +26,46 @@ def login():
         
     return render_template('login.html', error=error, username=username, password=password)
     
-    
 
 @lab4.route('/lab4/success2')
 def success2():
     username = request.form.get('username')
-    return render_template('success2.html')
+    return render_template('success2.html', username=username)
+
+
+@lab4.route('/lab4/holod', methods = ['GET', 'POST'])
+def holod():
+    if request.method == 'GET':
+        return render_template('holod.html')
+    
+    temp = request.form.get('temp')
+     
+    if not temp:
+        error3 = 'Ошибка: не задана температура'
+        return render_template('holod.html', temp=temp, error3=error3)
+    
+    temp = int(temp)
+    snowflakes = 0
+    if temp < -12 :
+        error3 = 'Не удалось установить температуру — слишком низкое значение'
+        snowflakes = 0
+        return render_template('holod.html', temp=temp, error3=error3, snowflakes=snowflakes)
+    elif temp > -1 :
+        error3 = 'Не удалось установить температуру — слишком высокое значение'
+        return render_template('holod.html', temp=temp, error3=error3, snowflakes=snowflakes)
+    elif temp >= -12 and temp <= -9:
+        error3 = 'Установлена температура: {}'.format(temp) + '°С' 
+        snowflakes = 3
+        return render_template('holod.html', temp=temp, error3=error3, snowflakes=snowflakes)
+    elif temp >= -8 and temp <= -5:
+        error3 = 'Установлена температура: {}'.format(temp) + '°С'  
+        snowflakes = 2
+        return render_template('holod.html', temp=temp, error3=error3, snowflakes=snowflakes)
+    elif temp >= -4 and temp <= -1:
+        error3 = 'Установлена температура: {}'.format(temp) + '°С' 
+        snowflakes = 1
+        return render_template('holod.html', temp=temp, error3=error3, snowflakes=snowflakes)
+    else:
+        return render_template('holod.html', temp=temp, error3=error3, snowflakes=snowflakes)
+
+    
