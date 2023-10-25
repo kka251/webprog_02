c5385dd0759e00f64a8fbb549c09dfd3a1b6d795
kka251 <k.kurilowa2018@yandex.ru>
2023-10-25 17:45:36 +0700

    Куки - цвет текста

--- lab4.py
+++ lab4.py
@@ -105,7 +105,22 @@ def zerno():
         return render_template('zerno.html', grain=grain, weight=weight, total_price=total_price, message=message)
     return render_template('zerno.html', grain=grain, weight=weight, total_price=total_price, zakaz=zakaz)
 
+@lab4.route('/lab4/cookies', methods = ['GET', 'POST'])
+def cookies():
+    if request.method == 'GET': 
+        return render_template('cookies.html')
+    
+    color = request.form.get('color')
+    
+    
+    headers = {
+        'Set-Cookie': [
+            'color=' + color + ';path=/'
+            ],
+        'Location': '/lab4/cookies'
+}
 
+    return '', 303, headers
 
     
 
