01d8046698789c7014883cdd1bcd97a5fa11aef8
kka251 <k.kurilowa2018@yandex.ru>
2023-12-20 11:27:05 +0700

    Открытка

--- lab9.py
+++ lab9.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 
 lab9 = Blueprint ("lab9", __name__)
 
@@ -17,3 +17,19 @@ def not_found(e):
 @lab9.route('/lab9/error500')
 def cause_error():
     return 500/0
+
+@lab9.route('/lab9/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        recipient_name = request.form.get('recipient_name')
+        recipient_gender = request.form.get('recipient_gender')
+        sender_name = request.form.get('sender_name')
+        return redirect(url_for('lab9.cart', recipient_name=recipient_name, recipient_gender=recipient_gender, sender_name=sender_name))
+    return render_template('lab9/index.html')
+
+@lab9.route('/lab9/cart/')
+def cart():
+    recipient_name = request.args.get('recipient_name')
+    recipient_gender = request.args.get('recipient_gender')
+    sender_name = request.args.get('sender_name')
+    return render_template('lab9/cart.html', recipient_name=recipient_name, recipient_gender=recipient_gender, sender_name=sender_name)
